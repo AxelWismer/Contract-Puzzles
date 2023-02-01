d53a10ceffc9ddad7b2afd62b42cff9dbab374ec
AxelWismer <mmowismer@outlook.com>
2023-02-01 13:06:23 -0300

    all tests resolved

--- test/game5Test.js
+++ test/game5Test.js
@@ -6,14 +6,24 @@ describe('Game5', function () {
     const Game = await ethers.getContractFactory('Game5');
     const game = await Game.deploy();
 
-    return { game };
+    let signer = ethers.provider.getSigner(0);
+    let wallet;
+    
+    // look for a wallet whose address match the contract requirements
+    do  {
+      wallet = ethers.Wallet.createRandom().connect(ethers.provider);
+    } while (wallet.address.slice(0,5) >= '0x00F')
+    
+    // Send funds
+    await signer.sendTransaction({to: wallet.address, value: ethers.utils.parseEther("1")});
+    return { game, wallet };
   }
+  
   it('should be a winner', async function () {
-    const { game } = await loadFixture(deployContractAndSetVariables);
+    const { game, wallet } = await loadFixture(deployContractAndSetVariables);
 
     // good luck
-
-    await game.win();
+    await game.connect(wallet).win();
 
     // leave this assertion as-is
     assert(await game.isWon(), 'You did not win the game');
